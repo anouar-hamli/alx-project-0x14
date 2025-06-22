96a05b917811022c0700fea95ebd0383611b8612
anouar hamli <anouarhamli10@gmail.com>
2025-06-22 22:50:00 +0100

    commit

--- alx-movie-app/components/commonsy/Button.tsx
+++ alx-movie-app/components/commonsy/Button.tsx
@@ -1,6 +1,11 @@
-// components/commons/Button.tsx
-const Button = () => {
-  return <button>Click me</button>;
+import React from 'react';
+
+const Button: React.FC = () => {
+  return (
+    <button className="bg-blue-500 text-white px-4 py-2 rounded">
+      Button
+    </button>
+  );
 };
 
 export default Button;
